df481d4afe450d8f829714893d092b8e0937c627
Amyas Merivale <merivale@gmail.com>
2018-01-21 11:49:11 +0100

    cosmetic changes

--- docs/js/bootstrap.js
+++ docs/js/bootstrap.js
@@ -1,15 +1,19 @@
 (function () {
   var toggles = document.querySelectorAll('[data-action="toggle-menu"]');
+  var nav = document.querySelector('nav');
+  var main = document.querySelector('main');
   var victor = document.getElementById('victor');
-  var toggle = function (event) {
-    event.preventDefault();
-    document.querySelector('nav').classList.toggle('open');
-    document.querySelector('main').classList.toggle('menu-open');
-  };
-  var addToggle = function (node) {
-    node.addEventListener('click', toggle);
-  };
-  Array.from(toggles).forEach(addToggle);
+  Array.from(toggles).forEach(function (node) {
+    node.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('nav').classList.toggle('open');
+      document.querySelector('main').classList.toggle('menu-open');
+    });
+  });
+  main.addEventListener('click', function (e) {
+    nav.classList.remove('open');
+    main.classList.remove('menu-open');
+  });
   if (victor) {
     Elm.Victor.embed(document.getElementById('victor'));
   }
